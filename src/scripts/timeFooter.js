6245aebbf202928737bdad1666d8d040ec83fff0
David <romao.carlosdavi@gmail.com>
2021-05-25 10:55:40 -0300

    script para controlar timer no footer e cor do nav

---
+++ src/scripts/timeFooter.js
@@ -0,0 +1,10 @@
+const span = document.querySelector("#ano")
+
+var data = new Date();
+
+
+span.innerHTML = data.getFullYear()
+
+const nav = document.querySelector("nav");
+
+nav.classList.add("navbar-dark")
