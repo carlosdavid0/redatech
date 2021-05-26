0f4aa41e827736d0b8b47b8f4327e2f82f5fb90b
David <romao.carlosdavi@gmail.com>
2021-05-26 17:34:12 -0300

    google analytics

--- src/scripts/timeFooter.js
+++ src/scripts/timeFooter.js
@@ -8,3 +8,9 @@ span.innerHTML = data.getFullYear()
 const nav = document.querySelector("nav");
 
 nav.classList.add("navbar-dark")
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'UA-50621360-1');
